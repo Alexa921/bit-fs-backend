134952c9371d3ebf03f43e1dad15440eaa970d7d
Alexa921 <alexandrarestrepoagudelo451@gmail.com>
2025-03-11 21:44:24 -0500

    Avances

--- src/schemas/productos.js
+++ src/schemas/productos.js
@@ -2,11 +2,17 @@ import mongoose from "mongoose";
 
 const productoSchema = new mongoose.Schema(
   {
+    imagen: { type: String, required: true },
     titulo: { type: String, required: true },
     precio: { type: Number, required: true },
-    imagen: { type: String, required: true },
+    categoria: {
+      type: String,
+      required: true,
+      enum: ["novedades", "hombre", "mujer", "niño", "descuentos"],
+      default: "novedades",
+    },
   },
   { timestamps: true, versionKey: false }
 );
 
-export default mongoose.model("productos", productoSchema);
+export default mongoose.model("novedades", productoSchema);
--- src/server.js
+++ src/server.js
@@ -1,16 +1,18 @@
-import express from "express";
-import morgan from "morgan";
-import rutasProductos from "./rutas/productos.js";
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+import rutasProductos from './rutas/productos.js';
 
 const servidor = express();
 
-servidor.use(morgan("dev"));
+servidor.use(morgan('dev'));
+servidor.use(cors());
 servidor.use(express.json());
 servidor.use(express.urlencoded({ extended: false }));
-servidor.use("/productos", rutasProductos);
+servidor.use('/productos', rutasProductos);
 
-servidor.get("/", (solcitud, respuesta) => {
-  respuesta.json({ mensaje: "raiz ok", data: null });
+servidor.get('/', (solicitud, respuesta) => {
+  respuesta.json({ mensaje: 'raíz ok', data: null });
 });
 
 export default servidor;
